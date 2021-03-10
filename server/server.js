345e61736d411b5ed7167a20878565d2f06a9463
Filomaster <fpm1504@gmail.com>
2021-03-10 14:34:08 +0100

    Added utils module and created base for server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+// Author: Filip 'Filomaster' Majorek
+// Description: This is main file of Ludo server
+
+const express = require("express");
+const session = require("express-session");
+const output = require("./library/utils").out;
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+// Starting server
+app.listen(PORT, () => {
+  output.info(`Started listening on port ${PORT}`);
+});
