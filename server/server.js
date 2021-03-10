e111fd67b42b4edac720c87d3e79c902d68963bc
Filomaster <fpm1504@gmail.com>
2021-03-10 15:35:19 +0100

    Fix for port on heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const session = require("express-session");
 const output = require("./library/utils").out;
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Starting server
 app.listen(PORT, () => {
